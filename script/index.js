901adc0abf4854c1b7cccd794485eb44113ea683
MUHAMMED KABUGO <KABUGOMUHA@GMAIL.COM>
2025-06-04 17:12:25 -0400

    project 4 final stage

--- script/index.js
+++ script/index.js
@@ -1,12 +1,33 @@
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__new-post-button");
 const newPostModal = document.querySelector("#new-post-modal");
+const editNewPostForm = newPostModal.querySelector(".modal__form");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+// Update the id
+const editNewPostImageInput = newPostForm.querySelector("#card-image-input");
+
+const editNewPostCaptionInput = editProfileModal.querySelector(
+  "#card-caption-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -21,3 +42,18 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDecscriptionInput.value;
+  editProfileModal.classList.remove("modal__is-opened");
+}
+
+function handleENewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(evt);
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+editNewPostForm.addEventListener("submit", handleENewPostSubmit);
